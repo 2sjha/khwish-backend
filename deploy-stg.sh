616736f70e536a0f0713a8b477c27aec3b08d891
Shubham Shekhar Jha <jhashubhamshekhar@gmail.com>
2022-08-24 20:04:43 -0500

    Update deploy-stg.sh

--- deploy-stg.sh
+++ deploy-stg.sh
@@ -1,2 +1,2 @@
 mvn clean install -DskipTests
-scp target/khwish-backend-0.0.1.jar ../khwish-firebase-service-account.json ../khwish-backend-service-account.json run-stg.sh ssj@139.59.3.37:/home/ssj/khwish/
+scp target/khwish-backend-0.0.1.jar ../khwish-firebase-service-account.json ../khwish-backend-service-account.json run-stg.sh ssj@xxx.xx.x.xx:/home/ssj/khwish/
